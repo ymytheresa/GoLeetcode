b770929fea648ec80417384f63348775b56d82b5
Theresa <38038286+ymytheresa@users.noreply.github.com>
2024-09-04 15:02:41 +0800

    Time: 11 ms (52.06%), Space: 6.2 MB (40.35%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,14 @@
+func twoSum(numbers []int, target int) []int {
+    l, r := 0, len(numbers)-1
+    for l < r {
+        sum := numbers[l] + numbers[r]
+        if sum == target{
+            return []int{l+1, r+1}
+        }else if sum > target {
+            r--
+        }else{
+            l++
+        }
+    }
+    return []int{}
+}
