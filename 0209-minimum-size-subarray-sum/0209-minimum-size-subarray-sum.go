09ad1798601cb3172a24758bf87149472c0a5e0a
Theresa <38038286+ymytheresa@users.noreply.github.com>
2024-09-05 09:31:35 +0800

    Time: 31 ms (17.14%), Space: 7.2 MB (91.59%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.go
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.go
@@ -1,19 +1,17 @@
 func minSubArrayLen(target int, nums []int) int {
-    res, sum, startIdx := math.MaxInt32, 0, 0
-    found := false
+    res, sum, startIdx := len(nums)+1, 0, 0
     for i, n := range nums{
         sum += n
         for sum >= target{
             res = min(res, i-startIdx+1)
-            found = true
             sum -= nums[startIdx]
             startIdx++
         }
     }
 
-    if found {
-        return res
-    }else {
+    if res == len(nums)+1 {
         return 0
+    }else {
+        return res
     }
 }
